81e78f7286750da3ee36ee851799f1732267db59
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-09-05 21:15:02 +0200

    crushing

---
+++ listing/activerecord_finders.rb
@@ -0,0 +1,8 @@
+User.first
+# SELECT * FROM "users" LIMIT 1
+User.first(:conditions => { :login => "johndoe" })
+# SELECT * FROM "users" WHERE
+#   ("users"."login" = 'johndoe') LIMIT 1
+User.find_by_login("johndoe")
+# SELECT * FROM "users" WHERE
+#   ("users"."login" = 'johndoe') LIMIT 1
