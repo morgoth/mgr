70f4ddfca338d155490113bb0544d10ada9fdeea
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-09-20 16:17:07 +0200

    authlogic

--- listing/4_sh_application.sh
+++ listing/4_sh_application.sh
@@ -3,7 +3,7 @@ sudo gem sources -a http://gems.github.com
 cp config/example_database.yml config/database.yml
 # instalacja Ruby on Rails
 sudo gem install rails
-# instalacja gema dla bazy danych sqlite3
+# instalacja gema dla bazy danych SQLite3
 sudo gem install sqlite3-ruby
 # instalacja używanych gemów
 sudo rake gems:install RAILS_ENV=test
